52fdefb3a15447bce239d92340d793dacd38aca9
Fish <okfish@yandex.ru>
2014-01-28 07:38:21 +0400

    Playing around faceted catalogue views based on Ashia's (@laidibug) branch (commit: 0dbd554d87051783180fc56391d0d6d28fab3615). With respects to :)
    There are some enhancements for Demo-site:
    
    - first attempt to implement multi-payment: 4 way, cash, invoice, paypal and datacash. Last two inherited, yep. In early development.
    
    - search_index and schema.xml extended with some fields:
    	size - for clothings to demonstrate grouped product attributes extraction;
    	author - to  show how to make alphabeted list of values;
    	and binding/publisher as an example of product attribute values collecting
    You need to add that fields in your settings.OSCAR_SEARCH_FACETS.
    (that was a tricky at the first view, FACET_ATTR_IDS is a hack, ugly but works)
    
    -facet.py knows about "show_zero" field in OSCAR_SEARCH_FACETS and collects zero-counted results from haystack for more flexible facet handling in templates.
    You need to add that key in your settings.OSCAR_SEARCH_FACETS['fields'].
    
    - templates adjusted to show alphabeted list of book's authors. Needs futher css-ing.

--- sites/demo/apps/search/search_indexes.py
+++ sites/demo/apps/search/search_indexes.py
@@ -1,33 +1,72 @@
 from haystack import indexes 
 from django.db.models import get_model
+from oscar.core.loading import get_class
+from django.core.exceptions import ImproperlyConfigured
 
 from oscar.apps.search.search_indexes import ProductIndex as CoreProductIndex 
+from settings import OSCAR_SEARCH_FACETS
+
+# Load default strategy (without a user/request)
+Selector = get_class('partner.strategy', 'Selector')
+strategy = Selector().strategy()
 
 ProductAttributeValue = get_model('catalogue', 'ProductAttributeValue')
+ProductAttribute = get_model('catalogue', 'ProductAttribute')
 
-#Foreign keys for m2m relations of each attribute. Ugly but works :) 
-#Maybe it makes sense to extend OSCAR_SEARCH_FACETS with that keys
-#Hardcoded to ProductAttributeValues table content
-ATTR_FKS = {
-            'publisher': 5,
-            'binding': 1,
-            }
+
+# Foreign keys for m2m relations of each attribute to extract values. Ugly but works :) 
+# Fill array with IDs of faceted attributes from ProductAttribute model depending
+# on settings 
+FACET_ATTR_IDS = {}
+
+for key, facet in OSCAR_SEARCH_FACETS['fields'].items():
+    try:
+        FACET_ATTR_IDS[facet['field']] = ProductAttribute.objects.get(code = facet['field']).id
+    except ProductAttribute.DoesNotExist: # If field is not attribute - pass it
+        pass    
 
 class ProductIndex(CoreProductIndex): 
     #Add Publisher and Binding attributes to index for faceting
+    author = indexes.CharField(null=True, faceted=True)
     publisher = indexes.CharField(null=True, faceted=True)
     binding = indexes.CharField(null=True, faceted=True)
+    size = indexes.MultiValueField(null=True, faceted=True)
     
+    # Size, color etc can be assigned only to variant product  
+    def prepare_size(self, obj):
+        if obj.is_group:
+            return [ self.get_attribute(v, 'size') for v in obj.variants.all() ]
+        else:
+            return self.get_attribute(obj, 'size')
+        
     def prepare_binding(self, obj):
-        return self.prepare_attribute(obj, 'binding')
+        if obj.is_group:
+             # Don't return a binding attr for group products
+             return None
+        else:        
+            return self.get_attribute(obj, 'binding')
+    
+    def prepare_author(self, obj):
+        if obj.is_group:
+             # Don't return a binding attr for group products
+             return None
+        else:        
+            return self.get_attribute(obj, 'author')
     
     def prepare_publisher(self, obj):
-        return self.prepare_attribute(obj, 'publisher')
+        if obj.is_group: # Don't return a publisher attr for group products
+            return None
+        else:
+            return self.get_attribute(obj, 'publisher')
 
-    def prepare_attribute(self, obj, attr_name):
+    def get_attribute(self, obj, attr_name):
         value = None
         try:
-            value = obj.attribute_values.get(attribute=ATTR_FKS[attr_name]).value
+            value = obj.attribute_values.get(attribute=FACET_ATTR_IDS[attr_name]).value
         except ProductAttributeValue.DoesNotExist:
             pass
+        except KeyError:
+            raise ImproperlyConfigured(("Attribute field '%s' not defined in settings"
+                                        " or in database. Add attribute via admin UI"
+                                        "or define it in settings.OSCAR_SEARCH_FACETS.") % attr_name)
         return value
